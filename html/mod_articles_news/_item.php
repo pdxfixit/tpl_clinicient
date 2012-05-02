bd50e206a8e85fa1d8c5730e4e0c7620b1a95b60
Ben Sandberg <ben@pdxfixit.com>
2012-05-02 23:24:22 +0000

    updated to 3.2.19
    git-svn-id: http://svn.pdxfixit.com/svn/clinicient@2 04a6b616-4c3c-bf46-af78-92371adf55c1

--- html/mod_articles_news/_item.php
+++ html/mod_articles_news/_item.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @package   Template Overrides - RocketTheme
- * @version   3.2.17 March 5, 2012
+ * @version   3.2.19 April 2, 2012
  * @author    RocketTheme http://www.rockettheme.com
  * @copyright Copyright (C) 2007 - 2012 RocketTheme, LLC
  * @license   http://www.gnu.org/licenses/gpl-2.0.html GNU/GPLv2 only
